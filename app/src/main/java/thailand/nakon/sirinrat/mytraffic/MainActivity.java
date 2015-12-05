3794f667fa2640d319dbef6fbd69cb92494b89a6
sirinrat <tanlove.3010@gmail.com>
2015-12-05 14:29:44 +0700

    ฉันได้ Button

--- app/src/main/java/thailand/nakon/sirinrat/mytraffic/MainActivity.java
+++ app/src/main/java/thailand/nakon/sirinrat/mytraffic/MainActivity.java
@@ -1,37 +1,52 @@
 package thailand.nakon.sirinrat.mytraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit การประกาศตัวแปร
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //bind widget
+        bindWidget();
+
+        //Button Controller
+        buttonController();
+
+    }// Main Method
+
+    private void buttonController() {
+
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            //Sound Effect
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.cow);
+                buttonMediaPlayer.start();
+            }//event
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void bindWidget() {
+
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-}
+} //Main class นี่คือคลาสหลัก

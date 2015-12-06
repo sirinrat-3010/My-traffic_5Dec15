aa56ed8c5340c5589f199e5bfaaf48746d2cc5a5
sirinrat <tanlove.3010@gmail.com>
2015-12-06 14:44:08 +0700

    เปลี่ยนหน้าแรก

--- app/src/main/java/thailand/nakon/sirinrat/mytraffic/SplashScreen.java
+++ app/src/main/java/thailand/nakon/sirinrat/mytraffic/SplashScreen.java
@@ -1,5 +1,7 @@
 package thailand.nakon.sirinrat.mytraffic;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,27 +13,18 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_splash_screen, menu);
-        return true;
-    }
+        Handler objHandler = new Handler();
+        objHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent objIntent = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(objIntent);
+                finish();
+            }
+        },3000);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    }//Main Method
 
-        return super.onOptionsItemSelected(item);
-    }
-}
+}//Main Class
